c72acfb621eab19149a91ffd42449b875e85b9d8
Corey Oordt <coreyoordt@gmail.com>
2011-10-12 17:15:25 -0400

    Updated the batch_unsub management command. Not sure it really matters.

--- newsletters/management/commands/batch_unsub.py
+++ newsletters/management/commands/batch_unsub.py
@@ -1,17 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
-from newsletters.models import Newsletter
+from newsletters.models import Subscription
     
 class Command(BaseCommand):
     help = "Batch unsubscribe users based on email"
     def handle(self, *args, **opts):
-        emailfile,slug = args
+        emailfile, slug = args
         newsletter = Newsletter.objects.get(slug=slug)
         for email in open(emailfile).readlines():
-            try:
-                user = User.objects.get(email=email.strip())
-            except User.DoesNotExist:
-                continue
-            profile = user.get_profile()
-            profile.newsletters.delete(newsletter)
-            profile.save()
+            Subscription.objects.filter(email=email).delete()
